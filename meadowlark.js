86dfc7a12f5a86d8ceb6675fb551434fd454c969
User <User@LAPTOP-1OFSPH9I>
2016-12-20 16:10:22 +0300

    9ch

--- meadowlark.js
+++ meadowlark.js
@@ -1,9 +1,10 @@
 var express = require('express'),
 	fortune = require('./lib/fortune.js'),
-	formidable = require('formidable'),
-	jqupload = require('jquery-file-upload-middleware');
+	formidable = require('formidable');
 
 var app = express();
+
+var credentials = require('./credentials.js');
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({
@@ -19,10 +20,25 @@ var handlebars = require('express-handlebars').create({
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-app.set('port', process.env.PORT || 8282);
+app.set('port', process.env.PORT || 8181);
 
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser')());
+
+// flash message middleware
+app.use(function(req, res, next){
+	// if there's a flash message, transfer
+	// it to the context, then clear it
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
 
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
@@ -67,19 +83,6 @@ app.use(function(req, res, next){
  	next();
 });
 
-// jQuery File Upload endpoint middleware
-app.use('/upload', function(req, res, next){
-    var now = Date.now();
-    jqupload.fileHandler({
-        uploadDir: function(){
-            return __dirname + '/public/uploads/' + now;
-        },
-        uploadUrl: function(){
-            return '/uploads/' + now;
-        },
-    })(req, res, next);
-});
-
 app.get('/', function(req, res) {
 	res.render('home');
 });
@@ -116,18 +119,51 @@ app.get('/thank-you', function(req, res){
 	res.render('thank-you');
 });
 app.get('/newsletter', function(req, res){
-    // we will learn about CSRF later...for now, we just
-    // provide a dummy value
-    res.render('newsletter', { csrf: 'CSRF token goes here' });
+	res.render('newsletter');
 });
-app.post('/process', function(req, res){
-    if(req.xhr || req.accepts('json,html')==='json'){
-        // if there were an error, we would send { error: 'error description' }
-        res.send({ success: true });
-    } else {
-        // if there were an error, we would redirect to an error page
-        res.redirect(303, '/thank-you');
-    }
+
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+	cb();
+};
+
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+	var name = req.body.name || '', email = req.body.email || '';
+	// input validation
+	if(!email.match(VALID_EMAIL_REGEX)) {
+		if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+		req.session.flash = {
+			type: 'danger',
+			intro: 'Validation error!',
+			message: 'The email address you entered was  not valid.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	}
+	new NewsletterSignup({ name: name, email: email }).save(function(err){
+		if(err) {
+			if(req.xhr) return res.json({ error: 'Database error.' });
+			req.session.flash = {
+				type: 'danger',
+				intro: 'Database error!',
+				message: 'There was a database error; please try again later.',
+			};
+			return res.redirect(303, '/newsletter/archive');
+		}
+		if(req.xhr) return res.json({ success: true });
+		req.session.flash = {
+			type: 'success',
+			intro: 'Thank you!',
+			message: 'You have now been signed up for the newsletter.',
+		};
+		return res.redirect(303, '/newsletter/archive');
+	});
+});
+app.get('/newsletter/archive', function(req, res){
+	res.render('newsletter/archive');
 });
 app.get('/contest/vacation-photo', function(req, res){
     var now = new Date();
