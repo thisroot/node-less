5fe053957778039e77746ad5dd92b7b7141575de
User <User@LAPTOP-1OFSPH9I>
2016-12-20 11:51:42 +0300

    8ch

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt){
 			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
 		},
 		exec: {
-			linkchecker: { cmd: 'blc http://less.nebesa.me -ro' }
+			linkchecker: { cmd: 'linkchecker --ignore-url=\'!^(https?:)\/\/localhost\b\' http://localhost:3000' }
 		},
 	});	
 
